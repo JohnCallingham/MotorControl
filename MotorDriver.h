44d001947ac198abfaa94d746d9315f4dabfe0f5
JohnCallingham <JohnCallingham@users.noreply.github.com>
2016-02-02 21:21:19 +0000

    Update MotorDriver.h

--- MotorDriver.h
+++ MotorDriver.h
@@ -1,8 +1,6 @@
 class MotorDriver {
 public:
-  MotorDriver(uint8_t addr = 0x40);
-  void begin(void);
-  void reset(void);
+  MotorDriver(uint8_t addr, int num);
   void setPWMFreq(float freq);
   void setPWM(uint8_t num, uint16_t on, uint16_t off);
 
